38258762224308ea5782f831f20130ec6eedc989
Zhanye <zhan.ye@iwhalecloud.com>
2024-11-10 17:27:28 +0800

    feat: -- 初始化预约挂号平台

---
+++ common/service_util/src/main/java/com/zy/yygh/handler/CommonMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.zy.yygh.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Mybatis plus Handler配置类
+ * @author starsea
+ * @date 2022-01-20
+ */
+@Component
+public class CommonMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
---
+++ model/src/main/java/com/zy/yygh/model/hosp/HospitalSet.java
@@ -0,0 +1,54 @@
+package com.zy.yygh.model.hosp;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.siro.yyds.model.base.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * <p>
+ * HospitalSet
+ * </p>
+ *
+ * @author qy
+ */
+@Data
+@ApiModel(description = "医院设置")
+@TableName("hospital_set")
+public class HospitalSet extends BaseEntity {
+
+	@ApiModelProperty(value = "医院名称")
+	@TableField("hosname")
+	private String hosname;
+
+	@ApiModelProperty(value = "医院编号")
+	@TableField("hoscode")
+	private String hoscode;
+
+	@ApiModelProperty(value = "api基础路径")
+	@TableField("api_url")
+	private String apiUrl;
+
+	@ApiModelProperty(value = "签名秘钥")
+	@TableField("sign_key")
+	private String signKey;
+
+	@ApiModelProperty(value = "联系人姓名")
+	@TableField("contacts_name")
+	private String contactsName;
+
+	@ApiModelProperty(value = "联系人手机")
+	@TableField("contacts_phone")
+	private String contactsPhone;
+
+	/**
+	 * 状态 1:可用 0：不可用
+	 */
+	@ApiModelProperty(value = "状态")
+	@TableField("status")
+	private Integer status;
+
+}
+
